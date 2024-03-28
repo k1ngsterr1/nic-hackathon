b020dc9a482e8aaa0948e60fb273c7984f63a8d0
Nruzhzhaugash <clonn262@gmail.com>
2024-03-28 11:45:41 +0500

    added layout component

---
+++ src/widgets/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+
+interface ILayout {
+  children: React.ReactNode;
+  type?: 'login' | 'profile' ;
+}
+
+export default function Layout({ children }: ILayout) {
+  return (
+    <article>
+      <Header />
+        {children}
+      <Footer />
+    </article>
+  )
+}
