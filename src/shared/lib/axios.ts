76247ed6dd3aeea9e38452f9154bb340cd808210
Abay <a.kosaev@inbox.ru>
2024-03-28 21:43:43 +0500

    axios

---
+++ src/shared/lib/axios.ts
@@ -0,0 +1,29 @@
+import axios, {AxiosError} from "axios";
+
+
+
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:8080", // URL will be changed by backend url
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("@access-token") || ""
+    if(config.headers && token) config.headers["Authorization"] = `Bearer ${token}`
+    return config
+})
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async(error: AxiosError) => {
+        if(error.response?.status === 401) {
+            localStorage.removeItem("@access-token")
+            // window.location.href = '/'  Что будем делать если получили статус 401 Unauthorized
+        }
+        return Promise.reject(error)
+    }
+)
+export default axiosInstance
