7708273f4d7313a599a31b3a349923d3220edb83
Nruzhzhaugash <clonn262@gmail.com>
2024-03-28 11:37:14 +0500

    feature

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,17 +1,31 @@
-import { ButtonHTMLAttributes } from "react";
+"use client"
+import React, { forwardRef, Ref } from 'react';
+import { motion } from 'framer-motion';
+import cn from 'classnames';
 
-interface IButton {
+interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
-  label?: string;
+  label: string;
 }
 
-export default function Button({ className, label, ...rest }: IButton) {
+const Button = forwardRef((props: IButton, ref: Ref<HTMLButtonElement>) => {
+  const { className, label, ...rest } = props;
   return (
-    <button 
-      className={className} 
+    <button
+      ref={ref}
+      className={cn(className, 'outline-none border-none')}
       {...rest}
     >
       {label}
     </button>
-  )
+  );
+});
+
+const MButton = motion(Button);
+
+if (process.env.NODE_ENV === 'development') {
+  const componentName = MButton.displayName || MButton.name || 'Component';
+  Button.displayName = `ForwardRef(${componentName})`;
 }
+
+export default MButton;
